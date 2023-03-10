da2dc629b16555d89c119f559c600102c6409072
delwerHossain <delwerhossain006@gmail.com>
2023-03-10 15:15:01 +0000

    fix

--- m-42/js/cookies.js
+++ m-42/js/cookies.js
@@ -8,17 +8,22 @@ document.getElementById("set-cookies").addEventListener("click", () => {
 document.getElementById("input-cookies-btn").addEventListener("click", () => {
   const name = document.getElementById("input-name").value;
   const value = document.getElementById("input-value").value;
-  document.getElementById("input-name").value = "";
-  document.getElementById("input-value").value = ""; 
-  localStorage.setItem(name, value);
+  if (name && value) {
+    document.getElementById("input-name").value = "";
+    document.getElementById("input-value").value = "";
+    localStorage.setItem(name, value);
     const get = localStorage.getItem(name);
-    getList(name , value);
-//   console.log(get);
+    getList(name, value);
+  }
+  else {
+    alert("Please enter name and value");
+  }
+  //   console.log(get);
 });
 
 function getList(name, value) {
-    const parent = document.getElementById("list");
-    const li = document.createElement("li");
-    li.textContent = `${name} : ${value}`;
-    parent.appendChild(li);
- }
+  const parent = document.getElementById("list");
+  const li = document.createElement("li");
+  li.textContent = `${name} : ${value}`;
+  parent.appendChild(li);
+}
