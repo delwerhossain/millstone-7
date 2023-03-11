1ba9f8e7506896cb198e81256781d3420d07b642
delwerHossain <delwerhossain006@gmail.com>
2023-03-11 10:42:35 +0000

    file name change

--- m-42/js/localStorage.js
+++ m-42/js/localStorage.js
@@ -1,14 +1,9 @@
-// document.getElementById("set-cookies").addEventListener("click", () => {
-//   const set = localStorage.setItem("user", "delwer hossain");
-//   const get = localStorage.getItem("user");
-//   console.log(get);
-//   // console.log(set);
-// });
-
-
-
-
-
+document.getElementById("set-cookies").addEventListener("click", () => {
+  const set = localStorage.setItem("user", "delwer hossain");
+  const get = localStorage.getItem("user");
+  console.log(get);
+  // console.log(set);
+});
 
 document.getElementById("input-cookies-btn").addEventListener("click", () => {
   const name = document.getElementById("input-name").value;
@@ -17,50 +12,46 @@ document.getElementById("input-cookies-btn").addEventListener("click", () => {
     document.getElementById("input-name").value = "";
     document.getElementById("input-value").value = "";
     setStorage(name, value);
-  }
-  else {
+  } else {
     alert("Please enter name and value");
   }
   displayProductsFromLocal();
 });
 
-
-
-
-// create obj 
+// create obj
 const getStorageShopping = () => {
   let cart = {};
-  let check = localStorage.getItem('cart');
+  let check = localStorage.getItem("cart");
   if (check) {
     cart = JSON.parse(check);
-  };
+  }
   return cart;
 };
- 
+
 // add to cart set in local storage
 const setStorage = (name, value) => {
   let cart = getStorageShopping();
   cart[name] = value;
   cart = JSON.stringify(cart);
-  localStorage.setItem('cart', cart);
+  localStorage.setItem("cart", cart);
 };
 
 // set list of attributes
 const getList = (name, value) => {
   const parent = document.getElementById("list");
- const li = document.createElement("li");
- li.textContent = `${name} : ${value}`;
- parent.appendChild(li);
+  const li = document.createElement("li");
+  li.textContent = `${name} : ${value}`;
+  parent.appendChild(li);
 };
 
+// DISPLA
 const displayProductsFromLocal = () => {
   let cart = getStorageShopping();
   document.getElementById("list").innerHTML = "";
   for (const value in cart) {
     console.log(value, cart[value]);
     getList(value, cart[value]);
-  };
+  }
 };
 
 displayProductsFromLocal();
-
